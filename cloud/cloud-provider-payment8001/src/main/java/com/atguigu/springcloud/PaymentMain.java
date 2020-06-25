48c2eb86da2dbc02f163b9b4ff69a798152fb3e9
小帅 <1543010660@qq.com>
2020-06-25 17:37:02 +0800

    springcloud 支付模块架构-d代码提交

--- cloud/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain.java
+++ cloud/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class PaymentMain {
+
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain.class, args);
     }
